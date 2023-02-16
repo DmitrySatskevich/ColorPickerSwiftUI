55d18d4b5f112e5d73a004b72ec8886a96ea233e
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-02-16 13:54:14 +0300

    Project created

---
+++ ColorPickerSwiftUI/ColorPickerSwiftUI/ColorPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSwiftUIApp.swift
+//  ColorPickerSwiftUI
+//
+//  Created by dzmitry on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
